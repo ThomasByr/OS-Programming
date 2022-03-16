2725775b717a8d7fe793aff19bde79c59e3c16f7
ThomasByr <tbouyer2000@gmail.com>
2022-03-16 17:14:17 +0100

    wp 1 (currently unrevised)

--- ex 1/exo1.s
+++ ex 1/exo1.s
@@ -1,4 +1,4 @@
-//! ./z33-cli run {__file__} {__function__}
+//! ./z33-cli run {__file__} {__function__} -{__o__} --{__option__}
 
 calcul:
     // x*x - 2*x*y - y*y
@@ -9,16 +9,16 @@ calcul:
     ld [%sp+3], %b           // y
 
     mul 2, %a
-    mul %b, %a               // 2xy
+    mul %b, %a               // 2*x*y
 
     ld [%sp+2], %b           // x
-    mul %b, %b               // xx
-    sub %a, %b               // xx - 2xy
+    mul %b, %b               // x*x
+    sub %a, %b               // x*x - 2*x*y
     swap %a, %b
 
     ld [%sp+3], %b           // y
-    mul %b, %b               // yy
-    sub %b, %a               // xx - 2xy - yy
+    mul %b, %b               // y*y
+    sub %b, %a               // x*x - 2*x*y - y*y
 
     pop %b
     rtn
@@ -31,27 +31,33 @@ prodscal:
     push 0                   // p
     push 0                   // i
     jmp prodscal_for
+
 prodscal_for:
     ld [%sp], %b             // i
     ld [%sp+6], %a           // n
     cmp %b, %a               // i >= n ?
     jge prodscal_end
+
     ld [%sp+4], %a           // int *v1
     add %b, %a               // v1[i]
     ld [%a], %a
     push %a
+
     ld [%sp+6], %a           // int *v2 (we pushed a before so +6)
     add %b, %a               // v2[i]
     ld [%a], %a
     pop %b
-    mul %b, %a               // v1[i] * v2[i]
+    mul %b, %a               // v1[i]*v2[i]
+
     ld [%sp+1], %b
-    add %a, %b               // p += v1[i] * v2[i]
-    st %b, [%sp+1]
+    add %a, %b
+    st %b, [%sp+1]           // p += v1[i]*v2[i]
+
     ld [%sp], %b
     add 1, %b                // i++
     st %b, [%sp]
     jmp prodscal_for
+
 prodscal_end:
     add 1, %sp
     pop %a
@@ -71,6 +77,7 @@ racine:
     div 2, %a
     push %a
     jmp racine_loop
+
 racine_loop:
     // 1st while condition
     ld [%sp+5], %a
@@ -79,6 +86,7 @@ racine_loop:
     cmp %b, %a               // n <= r*r ?
     jle racine_inwhile
     jmp racine_if
+
 racine_inwhile:
     // 2nd while condition
     ld [%sp], %b
@@ -87,6 +95,7 @@ racine_inwhile:
     cmp %b, %a               // n <= (r+1)*(r+1) ?
     jle racine_if
     jmp racine_end
+
 racine_if:
     // if r*r > n
     ld [%sp], %b
@@ -94,23 +103,28 @@ racine_if:
     ld [%sp+5], %a
     cmp %b, %a               // n <= r*r ?
     jle racine_else
+
     ld [%sp], %b
     st %b, [%sp+1]           // inf = r
+
     ld [%sp+1], %a
     ld [%sp+2], %b
     add %b, %a
     div 2, %a
     st %a, [%sp]             // r = (inf+sup) / 2
     jmp racine_loop
+
 racine_else:
     // if (r+1)*(r+1) <= n
     ld [%sp], %b
     st %b, [%sp+2]           // sup = r
+
     ld [%sp+1], %a
     add %b, %a
     div 2, %a
     st %a, [%sp]             // r = (inf+sup) / 2
     jmp racine_loop
+
 racine_end:
     pop %a
     add 2, %sp
@@ -120,7 +134,7 @@ racine_end:
 
 main_calcul:
     push 2
-    push 2
+    push 11
     call calcul
     reset
 
